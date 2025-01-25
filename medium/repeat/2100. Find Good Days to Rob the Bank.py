b8074daee66dadf89425e09c08a741595c762127
iftihorshukur <iftihor.shukur@gmail.com>
2025-01-25 21:41:28 +0500

    nothing

--- medium/repeat/2100. Find Good Days to Rob the Bank.py
+++ medium/repeat/2100. Find Good Days to Rob the Bank.py
@@ -1,7 +1,39 @@
 from typing import List
 
 
+# time limit
 class Solution:
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        if len(security) <= 2 * time:
+            return []
+        if time == 0:
+            return [i for i in range(len(security))]
+        result = []
+        for i in range(time, len(security) - time):
+            if security[i] > security[i - 1] or security[i] > security[i + 1]:
+                continue
+            good_day = True
+            tmp_num = security[i - time]
+            for num in security[i - time:i]:
+                if num > tmp_num:
+                    good_day = False
+                    break
+                tmp_num = num
+            if not good_day:
+                continue
+            tmp_num = security[i + 1]
+            for num in security[i + 1:i + time + 1]:
+                if num < tmp_num:
+                    good_day = False
+                    break
+                tmp_num = num
+
+            if good_day:
+                result.append(i)
+        return result
+
+
+class Solution2:
     def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
         if time == 0:
             return [i for i in range(len(security))]
@@ -23,5 +55,6 @@ class Solution:
 
         return result
 
-a = Solution()
-print(a.goodDaysToRobBank([5,3,3,3,5,6,2], 2))
+
+a = Solution2()
+print(a.goodDaysToRobBank([5, 3, 3, 3, 5, 6, 2], 2))
