8355dcb0be07b822b54b23592ef0475f10e68846
iftixor <i.shukursalimov@uzinfocom.uz>
2025-05-05 16:47:35 +0500

    nothing

--- easy/448. Find All Numbers Disappeared in an Array.py
+++ easy/448. Find All Numbers Disappeared in an Array.py
@@ -4,14 +4,29 @@ nums1 = [1, 1]
 nums2 = [4, 3, 2, 7, 8, 2, 3, 1, 9]
 
 
+# [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# [3, 2, 3, 4, 8, 2, 7, 1, 9]
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        full_lst = list(range(1, len(nums) + 1))
-        full_lst = set(full_lst)
-        nums = set(nums)
-        full_lst -= nums
-        return list(full_lst)
+        return list(set(range(1, len(nums) + 1)) - set(nums))
 
 
-a = Solution()
+class Solution2:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        i = 0
+        result = []
+        while i < len(nums):
+            pos = nums[i] - 1
+            if nums[i] != nums[pos]:
+                nums[i], nums[pos] = nums[pos], nums[i]
+            else:
+                i += 1
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                result.append(i + 1)
+        return result
+
+
+a = Solution2()
 print(a.findDisappearedNumbers(nums2))
